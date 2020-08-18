008ddb08be409e725819d8995972cc0674da126c
arathjz <me@arathjz.com>
2020-08-18 18:56:49 -0500

    feat(setup): add tailwind config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
-  plugins: [`gatsby-plugin-typescript`]
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [require('tailwindcss')('./tailwind.config.js')]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: { tailwind: true }
+    }
+  ]
 };
