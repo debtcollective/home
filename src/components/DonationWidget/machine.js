3a3fd48f143d2466f6afd536f013f62f3ab9978c
Alexis Duran <alexis.duranm@gmail.com>
2020-09-03 22:07:25 +0200

    feat(donation-widget): add entry actions on first step of the machine
    this will allow to set the global context of "donationType"

--- src/components/DonationWidget/machine.js
+++ src/components/DonationWidget/machine.js
@@ -8,78 +8,95 @@ const sendDonation = async () => fetch('path/to/stripe');
  *
  * https://xstate.js.org/viz/?gist=50ecf807d3b9c049fc58cda690f90594
  */
-export const donationWizardMachine = Machine({
-  id: 'donation',
-  initial: 'amountForm',
-  states: {
-    amountForm: {
-      initial: 'donateOnce',
-      states: {
-        donateOnce: {
-          on: {
-            NEXT: '#donation.paymentForm',
-            'START.MONTHLY': 'donateMonthly'
-          }
-        },
-        donateMonthly: {
-          on: {
-            NEXT: '#donation.paymentForm',
-            'START.ONCE': 'donateOnce'
-          }
-        },
-        hist: {
-          type: 'history',
-          history: 'shallow'
-        }
-      }
+export const donationWizardMachine = Machine(
+  {
+    id: 'donation',
+    context: {
+      donationType: 'oneTime'
     },
-    paymentForm: {
-      initial: 'cardForm',
-      states: {
-        cardForm: {
-          on: {
-            NEXT: [{ target: 'addressForm', cond: 'paymentFormCompleted' }],
-            PREV: '#donation.amountForm.hist',
-            'START.ONCE': '#donation.amountForm.donateOnce',
-            'START.MONTHLY': '#donation.amountForm.donateMonthly'
-          }
-        },
-        addressForm: {
-          on: {
-            NEXT: [
-              {
-                target: '#donation.processDonation',
-                cond: 'addressFormCompleted'
-              }
-            ],
-            PREV: 'cardForm',
-            'START.ONCE': '#donation.amountForm.donateOnce',
-            'START.MONTHLY': '#donation.amountForm.donateMonthly'
+    initial: 'amountForm',
+    states: {
+      amountForm: {
+        initial: 'donateOnce',
+        states: {
+          donateOnce: {
+            on: {
+              entry: ['setMonthlyDonation'],
+              NEXT: '#donation.paymentForm',
+              'START.MONTHLY': 'donateMonthly'
+            }
+          },
+          donateMonthly: {
+            on: {
+              entry: ['setOneTimeDonation'],
+              NEXT: '#donation.paymentForm',
+              'START.ONCE': 'donateOnce'
+            }
+          },
+          hist: {
+            type: 'history',
+            history: 'shallow'
           }
         }
-      }
-    },
-    processDonation: {
-      invoke: {
-        id: 'submitDonation',
-        src: (context, event) => sendDonation(context.userId),
-        onDone: {
-          target: 'success',
-          actions: assign({ donation: (context, event) => event.data })
-        },
-        onError: {
-          target: 'failure',
-          actions: assign({ error: (context, event) => event.data })
+      },
+      paymentForm: {
+        initial: 'cardForm',
+        states: {
+          cardForm: {
+            on: {
+              NEXT: [{ target: 'addressForm', cond: 'paymentFormCompleted' }],
+              PREV: '#donation.amountForm.hist',
+              'START.ONCE': '#donation.amountForm.donateOnce',
+              'START.MONTHLY': '#donation.amountForm.donateMonthly'
+            }
+          },
+          addressForm: {
+            on: {
+              NEXT: [
+                {
+                  target: '#donation.processDonation',
+                  cond: 'addressFormCompleted'
+                }
+              ],
+              PREV: 'cardForm',
+              'START.ONCE': '#donation.amountForm.donateOnce',
+              'START.MONTHLY': '#donation.amountForm.donateMonthly'
+            }
+          }
+        }
+      },
+      processDonation: {
+        invoke: {
+          id: 'submitDonation',
+          src: (context, event) => sendDonation(context.userId),
+          onDone: {
+            target: 'success',
+            actions: assign({ donation: (context, event) => event.data })
+          },
+          onError: {
+            target: 'failure',
+            actions: assign({ error: (context, event) => event.data })
+          }
+        }
+      },
+      success: {
+        type: 'final'
+      },
+      failure: {
+        on: {
+          RETRY: { target: '#donation.paymentForm.addressForm' }
         }
       }
-    },
-    success: {
-      type: 'final'
-    },
-    failure: {
-      on: {
-        RETRY: { target: '#donation.paymentForm.addressForm' }
+    }
+  },
+  {
+    actions: {
+      setMonthlyDonation: () => {
+        assign({ donationType: 'monthly' });
+      },
+      setOneTimeDonation: () => {
+        assign({ donationType: 'oneTime' });
       }
     }
   }
-});
+);
