400f486e22449676702a7a6f75438cbbc3b554aa
Alexis Duran <alexis.duranm@gmail.com>
2020-09-03 22:05:28 +0200

    chore(donation-widget): add base to define state machine
    this commit will introduce the first base of the state machine for the donation workflow

---
+++ src/components/DonationWidget/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DonationWidget = () => {
+  return <div>Widget</div>;
+};
+
+export default DonationWidget;
---
+++ src/components/DonationWidget/machine.js
@@ -0,0 +1,85 @@
+import { Machine, assign } from 'xstate';
+
+const sendDonation = async () => fetch('path/to/stripe');
+
+/**
+ * A state machine to describe the transitions within the
+ * donation workflow.
+ *
+ * https://xstate.js.org/viz/?gist=50ecf807d3b9c049fc58cda690f90594
+ */
+export const donationWizardMachine = Machine({
+  id: 'donation',
+  initial: 'amountForm',
+  states: {
+    amountForm: {
+      initial: 'donateOnce',
+      states: {
+        donateOnce: {
+          on: {
+            NEXT: '#donation.paymentForm',
+            'START.MONTHLY': 'donateMonthly'
+          }
+        },
+        donateMonthly: {
+          on: {
+            NEXT: '#donation.paymentForm',
+            'START.ONCE': 'donateOnce'
+          }
+        },
+        hist: {
+          type: 'history',
+          history: 'shallow'
+        }
+      }
+    },
+    paymentForm: {
+      initial: 'cardForm',
+      states: {
+        cardForm: {
+          on: {
+            NEXT: [{ target: 'addressForm', cond: 'paymentFormCompleted' }],
+            PREV: '#donation.amountForm.hist',
+            'START.ONCE': '#donation.amountForm.donateOnce',
+            'START.MONTHLY': '#donation.amountForm.donateMonthly'
+          }
+        },
+        addressForm: {
+          on: {
+            NEXT: [
+              {
+                target: '#donation.processDonation',
+                cond: 'addressFormCompleted'
+              }
+            ],
+            PREV: 'cardForm',
+            'START.ONCE': '#donation.amountForm.donateOnce',
+            'START.MONTHLY': '#donation.amountForm.donateMonthly'
+          }
+        }
+      }
+    },
+    processDonation: {
+      invoke: {
+        id: 'submitDonation',
+        src: (context, event) => sendDonation(context.userId),
+        onDone: {
+          target: 'success',
+          actions: assign({ donation: (context, event) => event.data })
+        },
+        onError: {
+          target: 'failure',
+          actions: assign({ error: (context, event) => event.data })
+        }
+      }
+    },
+    success: {
+      type: 'final'
+    },
+    failure: {
+      on: {
+        RETRY: { target: '#donation.paymentForm.addressForm' }
+      }
+    }
+  }
+});
