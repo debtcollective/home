f968919dd8d43b7e07816f323d5c2dfe91104a12
arathjz <me@arathjz.com>
2020-09-09 19:16:58 -0500

    feat(landing-page): remove use of LineBreak

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,37 @@
 import React from 'react';
-import { navigate, graphql } from 'gatsby';
+import { navigate } from 'gatsby';
 import Layout from '@components/Layout';
 import Hero from '@components/Hero';
 import MainVictories from '@sections/MainVictories';
 import MainSlider from '@sections/MainSlider';
-import LineBreak from '@components/LineBreak';
 
-interface Props {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  data: any;
-}
-
-const Home: React.FC<Props> = ({ data }) => {
-  const heroImage = data?.heroImage?.childImageSharp?.fluid?.src || '';
-  const mainVictoriesImage =
-    data?.mainVictoriesImage?.childImageSharp?.fluid?.src || '';
-
+const Home: React.FC = () => {
   return (
     <Layout>
       <Hero
-        backgroundSrc={heroImage}
         title={
           <>
-            Alone our debts are a <LineBreak /> burden. Together they{' '}
-            <LineBreak /> make us <span className="text-yellow">powerful.</span>
+            Alone our debts are a burden. Together they make us{' '}
+            <span className="text-yellow">powerful.</span>
           </>
         }
         text={
           <>
             We are a debtors&apos; union fighting to cancel debts and defend
-            millions <LineBreak /> of households. Join us to build a world where
-            college is publicly <LineBreak /> funded, healthcare is universal
-            and housing is guaranteed for all.
+            millions of households. Join us to build a world where college is
+            publicly funded, healthcare is universal and housing is guaranteed
+            for all.
           </>
         }
         primaryAction={() => navigate('/debt-union')}
         primaryActionLabel="Join the Union"
-        secondaryAction={() => navigate('/debt-union')}
+        secondaryAction={() => navigate('/debt-union#membership-benefits')}
         secondaryActionLabel="Member Benefits"
-        hasActions
       />
       <MainSlider />
-      <MainVictories mainVictoriesImage={mainVictoriesImage} />
+      <MainVictories />
     </Layout>
   );
 };
-
-export const query = graphql`
-  query {
-    heroImage: file(relativePath: { eq: "main-hero.png" }) {
-      childImageSharp {
-        fluid(quality: 100) {
-          src
-        }
-      }
-    }
-    mainVictoriesImage: file(relativePath: { eq: "main-victories-image.png" }) {
-      childImageSharp {
-        fluid(quality: 100) {
-          src
-        }
-      }
-    }
-  }
-`;
 
 export default Home;
