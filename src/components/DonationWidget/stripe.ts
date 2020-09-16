d78d6d1975a474fdb6e8cc73b6fd41611ff19049
Alexis Duran <alexis.duranm@gmail.com>
2020-09-16 21:10:43 +0200

    feat(donation-widget): create a method to get stripe token options

--- src/components/DonationWidget/stripe.ts
+++ src/components/DonationWidget/stripe.ts
@@ -1,7 +1,29 @@
+import { DonationMachineContext } from './machine/types';
+
 /**
  * Publishable key that is used in order to load Stripe
  */
 export const STRIPE_API_KEY = 'pk_test_TYooMQauvdEDq54NiTphI7jx';
+
+/**
+ * Add extra data to the create token process to take advantage
+ * of Stripe built-in checks.
+ * https://stripe.com/docs/js/tokens_sources/create_token?type=cardElement#stripe_create_token-data
+ */
+export const getStripeTokenOptions = (
+  donationMachineContext: DonationMachineContext
+) => {
+  const { billingInformation, cardInformation } = donationMachineContext;
+
+  return {
+    name: `${cardInformation.firstName} ${cardInformation.lastName}`,
+    address_line1: billingInformation.address,
+    address_city: billingInformation.city,
+    address_zip: billingInformation.zipCode,
+    address_country: billingInformation.country,
+    currency: 'USD'
+  };
+};
 
 /**
  * CSS-in-JS styles for the card component
