39684c775233825f027c27465601643fc20eb339
Alexis Duran <alexis.duranm@gmail.com>
2020-09-19 13:45:16 +0200

    feat(donation-monthly): integrate quick option selection

--- src/components/DonationWidget/stories/DonationMonthlyForm.stories.tsx
+++ src/components/DonationWidget/stories/DonationMonthlyForm.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import faker from 'faker';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { DonationMonthlyForm } from '../components';
 import { Props as DonationMonthlyFormProps } from '../components/DonationMonthlyForm';
@@ -18,5 +17,5 @@ const MonthlyFormTemplate: Story<DonationMonthlyFormProps> = (args) => (
 export const MonthlyForm = MonthlyFormTemplate.bind({});
 
 MonthlyForm.args = {
-  defaultValues: { amount: faker.random.number(100) }
+  defaultValues: { amount: 20 }
 };
