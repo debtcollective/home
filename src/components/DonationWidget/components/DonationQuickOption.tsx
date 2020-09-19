fd10a5913c92d7302a17e9f1f065323104d05ad5
Alexis Duran <alexis.duranm@gmail.com>
2020-09-19 13:15:01 +0200

    feat(donation-quick-option): add new component to handle quick option selection

---
+++ src/components/DonationWidget/components/DonationQuickOption.tsx
@@ -0,0 +1,117 @@
+import React, { useRef } from 'react';
+import tw from 'twin.macro';
+import styled from 'styled-components';
+import { Input } from './DonationWizard';
+
+export interface Props {
+  name: string;
+  options: Array<number | 'other'>;
+  defaultChecked?: number;
+}
+
+const DonationQuickOption: React.FC<Props> = ({
+  options,
+  name,
+  defaultChecked
+}) => {
+  const groups = Math.ceil(options.length / 3);
+  const rows = Array(groups)
+    .fill('')
+    .map((_, i) => options.slice(i * 3, (i + 1) * 3));
+
+  const inputOther = useRef<HTMLInputElement>(null);
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.id.includes('other') && inputOther.current) {
+      inputOther.current.focus();
+    }
+  };
+
+  return (
+    <QuickOptionContainer>
+      {rows.map((row, i) => (
+        <QuickOptionRow key={`row-${i}`}>
+          {row.map((option) => {
+            return (
+              <QuickOption key={`key-${option}`}>
+                <input
+                  defaultChecked={option === defaultChecked}
+                  id={`option-${option}`}
+                  name={name}
+                  type="radio"
+                  value={option}
+                  onChange={handleOnChange}
+                />
+                {option === 'other' && (
+                  <Input
+                    ref={inputOther}
+                    id="option-other-input"
+                    name="other"
+                    placeholder="Other"
+                    title="Other"
+                    type="text"
+                  />
+                )}
+                <label htmlFor={`option-${option}`}>{option}</label>
+              </QuickOption>
+            );
+          })}
+        </QuickOptionRow>
+      ))}
+    </QuickOptionContainer>
+  );
+};
+
+export default DonationQuickOption;
+
+const QuickOptionRow = styled.div`
+  ${tw`flex flex-wrap justify-between -ml-2 mt-2`}
+`;
+
+const QuickOption = styled.div`
+  ${tw`flex w-1/3 last:flex-1 ml-2`}
+
+  label {
+    ${tw`
+      bg-white-100
+      block
+      border
+      border-beige-500
+      capitalize
+      cursor-pointer
+      focus:border-blue
+      focus:outline-none
+      py-2 px-3
+      rounded-md
+      text-center
+      w-full
+    `}
+  }
+
+  input {
+    ${tw`appearance-none`}
+
+    &:checked + label {
+      ${tw`bg-blue border-blue`}
+    }
+  }
+
+  #option-other {
+    &-input {
+      ${tw`hidden mt-0`}
+    }
+
+    &:checked ~ label {
+      ${tw`hidden`}
+    }
+
+    &:checked ~ #option-other-input {
+      ${tw`block`}
+    }
+  }
+`;
+
+const QuickOptionContainer = styled.div`
+  ${QuickOptionRow}:firt-child {
+    ${tw`mt-0`}
+  }
+`;
