f85949629ce7ec44e33faad978b480f493393315
Alexis Duran <alexis.duranm@gmail.com>
2020-09-25 11:00:09 +0200

    chore(heros): make sure to keep previous max-width 8xl

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -66,30 +66,32 @@ const Hero: React.FC<Props> = ({
     <BackgroundImage
       fluid={backgroundArtDirectionStack}
       className={classnames(
-        'min-h-section-size bg-no-repeat bg-cover bg-center -mt-20 pt-20 p-4 pb-8 md:px-12 lg:p-24',
+        'h-section-size max-h-section-max-size bg-no-repeat bg-cover bg-center -mt-20 pt-20 p-4 pb-8 md:px-12 lg:px-24 lg:pb-24',
         className
       )}
     >
-      <div className="mt-0 md:mt-16 max-w-3xl mr-auto">
-        <div className="flex flex-col">
-          <h1 className="font-bold text-5xl text-white md:text-6xl leading-none mt-4 md:mt-0">
-            {title}
-          </h1>
-          <p className="font-semibold text-xl text-white md:text-2xl leading-tight mt-4">
-            {text}
-          </p>
-        </div>
-        <div className="flex flex-col md:flex-row mt-12 md:max-w-xl">
-          <Button className="w-full md:w-1/2" onClick={primaryAction}>
-            {primaryActionLabel}
-          </Button>
-          <Button
-            className="w-full md:w-1/2 mt-4 md:mt-0 md:ml-4"
-            onClick={secondaryAction}
-            variant="secondary"
-          >
-            {secondaryActionLabel}
-          </Button>
+      <div className="max-w-8xl mx-auto">
+        <div className="mt-0 md:mt-16 max-w-3xl mr-auto">
+          <div className="flex flex-col">
+            <h1 className="font-bold text-5xl text-white md:text-6xl leading-none mt-4 md:mt-0">
+              {title}
+            </h1>
+            <p className="font-semibold text-xl text-white md:text-2xl leading-tight mt-4">
+              {text}
+            </p>
+          </div>
+          <div className="flex flex-col md:flex-row mt-12 md:max-w-xl">
+            <Button className="w-full md:w-1/2" onClick={primaryAction}>
+              {primaryActionLabel}
+            </Button>
+            <Button
+              className="w-full md:w-1/2 mt-4 md:mt-0 md:ml-4"
+              onClick={secondaryAction}
+              variant="secondary"
+            >
+              {secondaryActionLabel}
+            </Button>
+          </div>
         </div>
       </div>
     </BackgroundImage>
--- src/sections/DonateCover/index.tsx
+++ src/sections/DonateCover/index.tsx
@@ -46,18 +46,22 @@ const DonateCover: React.FC = () => {
     <>
       <BackgroundImage
         fluid={backgroundArtDirectionStack}
-        className="min-h-section-size bg-no-repeat bg-cover bg-center -mt-20 pt-20 p-4 pb-8 md:px-12 lg:p-24"
+        className="h-section-size max-h-section-max-size bg-no-repeat bg-cover bg-center -mt-20 pt-20 p-4 pb-8 md:px-12 lg:p-24"
       >
-        <div className="mt-0 md:mt-16 max-w-2xl ml-auto">
-          <h1 className="font-bold text-5xl text-white md:text-6xl md:text-right leading-none mt-4">
-            Help us <br className="md:hidden" />{' '}
-            <span className="text-yellow">build a Debtors&apos; movement!</span>
-          </h1>
-          <p className="font-semibold text-xl text-white md:text-right md:text-2xl leading-tight mt-4 mb-8">
-            Your contribution helps grow the union of debtors to fight back
-            against creditors and create the just economic system we deserve
-          </p>
-          <DonationWidget className="mr-auto ml-0 md:ml-auto md:mr-0" />
+        <div className="max-w-8xl mx-auto">
+          <div className="mt-0 md:mt-16 max-w-2xl ml-auto">
+            <h1 className="font-bold text-5xl text-white md:text-6xl md:text-right leading-none mt-4">
+              Help us <br className="md:hidden" />{' '}
+              <span className="text-yellow">
+                build a Debtors&apos; movement!
+              </span>
+            </h1>
+            <p className="font-semibold text-xl text-white md:text-right md:text-2xl leading-tight mt-4 mb-8">
+              Your contribution helps grow the union of debtors to fight back
+              against creditors and create the just economic system we deserve
+            </p>
+            <DonationWidget className="mr-auto ml-0 md:ml-auto md:mr-0" />
+          </div>
         </div>
       </BackgroundImage>
     </>
