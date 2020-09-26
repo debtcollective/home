c5c618648de5eee35b963449c440ae523fd762a6
Alexis Duran <alexis.duranm@gmail.com>
2020-09-26 15:17:11 +0200

    feat(header): fetch membership current user to display hub link

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import Footer from '@components/Footer';
 import SEO from '@components/SEO';
 import logoBlack from '@static/logo-black.png';
@@ -23,6 +23,7 @@ declare global {
 
 export const HOST_URL = process.env.HOST_URL;
 export const COMMUNITY_URL = process.env.COMMUNITY_URL;
+export const MEMBERSHIP_URL = `${process.env.MEMBERSHIP_URL}`;
 
 const HEADER_LINKS = [
   {
@@ -39,22 +40,55 @@ const HEADER_LINKS = [
   }
 ];
 
+const HUB_LINK = {
+  href: '/hub',
+  text: 'Member hub'
+};
+
+type MembershipStatus = 'pending' | 'active' | null;
+
 const Layout: React.FC<Props> = ({
   children,
   title,
   description,
   hideNewsletter
 }) => {
+  const [links, setLinks] = useState(JSON.stringify(HEADER_LINKS));
+
+  const onMembershipFetched = (response: { status: MembershipStatus }) => {
+    if (response.status !== null) {
+      setLinks(JSON.stringify([HUB_LINK, ...HEADER_LINKS]));
+      return;
+    }
+
+    setLinks(JSON.stringify(HEADER_LINKS));
+  };
+
+  useEffect(() => {
+    fetch(`${MEMBERSHIP_URL}/users/current.json`, {
+      // credentials: 'include',
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(async (res) => {
+      const response = await res.json();
+      onMembershipFetched(response);
+    });
+  }, []);
+
   return (
     <>
       <dc-header
+        id="dc-header"
         logo={logoBlack}
         logosmall={logoSmall}
         host={HOST_URL}
         memberhuburl={`${HOST_URL}/hub`}
         community={COMMUNITY_URL}
         donateurl="/donate"
-        links={JSON.stringify(HEADER_LINKS)}
+        links={links}
       ></dc-header>
       <SEO title={title} description={description} />
       <main className="mt-20">{children}</main>
