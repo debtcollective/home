67c8296def3e80d7500ac6b8d909122bea53e584
Alexis Duran <alexis.duranm@gmail.com>
2020-10-05 18:22:25 +0200

    fix(donation-hero): move content to left side

--- src/sections/DonateCover/index.tsx
+++ src/sections/DonateCover/index.tsx
@@ -50,18 +50,18 @@ const DonateCover: React.FC = () => {
       >
         <div className="absolute inset-0 bg-gray opacity-75"></div>
         <div className="relative max-w-full w-8xl mx-auto mt-8 md:mt-0">
-          <div className="py-10 md:pt-16 lg:pb-24 max-w-3xl ml-auto">
-            <h1 className="font-bold text-5xl text-white md:text-6xl md:text-right leading-none max-w-2xl ml-auto">
+          <div className="py-10 md:pt-16 lg:pb-24 max-w-3xl">
+            <h1 className="font-bold text-5xl text-white md:text-6xl leading-none max-w-2xl">
               Help us <br className="md:hidden" />{' '}
               <span className="text-yellow">
                 build a Debtors&apos; movement!
               </span>
             </h1>
-            <p className="font-semibold text-xl text-white md:text-right md:text-2xl leading-tight mt-4 mb-8">
+            <p className="font-semibold text-xl text-white md:text-2xl leading-tight mt-4 mb-8">
               Your contribution helps grow the union of debtors to fight back
               against creditors and create the just economic system we deserve
             </p>
-            <DonationWidget className="mr-auto ml-0 md:ml-auto md:mr-0" />
+            <DonationWidget className="ml-0 mr-auto md:mr-0" />
           </div>
         </div>
       </BackgroundImage>
