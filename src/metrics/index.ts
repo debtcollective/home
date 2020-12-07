8a4024f2bae766b53c0a9c349eea0376187f1bdb
Alexis Duran <alexis.duranm@gmail.com>
2020-12-07 13:07:49 +0100

    feat(metrics): add module to handle metrics iteraction

---
+++ src/metrics/index.ts
@@ -0,0 +1,30 @@
+enum GoogleAnalyticEventName {
+  ONE_OFF_DONATION = 'one_off_donation'
+}
+
+type GoogleAnalyticEvent = {
+  event?: GoogleAnalyticEventName;
+};
+
+const dl: Array<GoogleAnalyticEvent> = (window as any).dataLayer || [];
+
+/**
+ * Allows to send events to google tag manager for further analysis over
+ * our google analytics dashboard https://developers.google.com/tag-manager/devguide
+ *
+ * @param eventName the name of the event to push to the data layer
+ * @param opts any other param to enrich intended event
+ */
+const sendEvent = (eventName: GoogleAnalyticEventName, opts = {}) => {
+  dl.push({
+    event: eventName,
+    ...opts
+  });
+};
+
+const metrics = {
+  sendEvent,
+  ...GoogleAnalyticEventName
+};
+
+export default metrics;
