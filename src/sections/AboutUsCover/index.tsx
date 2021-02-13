ddb08cde3103660035c5cd88993639f954b14c1f
arathjz <me@arathjz.com>
2021-02-12 18:41:36 -0600

    feat(about-us): create cover component

---
+++ src/sections/AboutUsCover/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
+
+const AboutUsCover: React.FC = () => {
+  const { desktop, medium, small } = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "heros/about-us.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1920, quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      medium: file(relativePath: { eq: "heros/about-us.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1400, quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      small: file(relativePath: { eq: "heros/about-us.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 490, quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+  // Art-Direction Array
+  const backgroundArtDirectionStack = [
+    small.childImageSharp.fluid,
+    {
+      ...medium.childImageSharp.fluid,
+      media: `(min-width: 491px)`
+    },
+    {
+      ...desktop.childImageSharp.fluid,
+      media: `(min-width: 1401px)`
+    }
+  ];
+
+  return (
+    <>
+      <BackgroundImage
+        fluid={backgroundArtDirectionStack}
+        className="flex items-center px-4 bg-center bg-no-repeat bg-cover min-h-screen-45 md:px-12 lg:px-24"
+      >
+        <div className="absolute inset-0 opacity-50 bg-gray"></div>
+        <div className="relative max-w-full mx-auto w-8xl">
+          <h1 className="text-5xl font-bold leading-none text-white md:text-6xl">
+            About <br className="md:hidden" />{' '}
+            <span className="text-yellow">Us!</span>
+          </h1>
+        </div>
+      </BackgroundImage>
+    </>
+  );
+};
+
+export default AboutUsCover;
