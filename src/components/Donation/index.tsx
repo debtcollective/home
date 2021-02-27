06ccf87abd564d3e92ee959e1fe6a9262c49baaf
Alexis Duran <alexis.duranm@gmail.com>
2021-02-27 13:59:50 +0100

    refactor(donation): migrate to use @debtcollective/union-component

--- src/components/Donation/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as DonationWidget } from './DonationWidget';
-export type { Props as DonationWidgetProps } from './DonationWidget';
-export { default as MembershipWidget } from './MembershipWidget';
-export type { Props as MembershipWidgetProps } from './MembershipWidget';
