2e910e32be957fc5bed67f42512f79d3d4000f6f
Orlando Del Aguila <orlando@hashlabs.com>
2021-03-11 18:00:51 -0600

    dev: move global variables to gastby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,11 @@ import './src/tailwind.css';
 
 defineCustomElementsHeader();
 defineCustomElementsFooter();
+
+if (typeof window !== 'undefined') {
+  // TODO: We need to add support to pass these variables as props
+  window.DC_DONATE_API_URL = process.env.GATSBY_DONATE_API_URL;
+  window.DC_MEMBERSHIP_API_URL = process.env.GATSBY_MEMBERSHIP_API_URL;
+  window.DC_RECAPTCHA_V3_SITE_KEY = process.env.GATSBY_RECAPTCHA_V3_SITE_KEY;
+  window.DC_STRIPE_PUBLIC_TOKEN = process.env.GATSBY_STRIPE_PUBLIC_TOKEN;
+}
