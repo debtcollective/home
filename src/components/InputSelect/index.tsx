81ecfe12b12f9868c9658f4ea06c84de5c5e45ec
arathjz <me@arathjz.com>
2021-04-21 17:12:07 -0500

    dev(input-select): update component onchange api

--- src/components/InputSelect/index.tsx
+++ src/components/InputSelect/index.tsx
@@ -5,8 +5,13 @@ interface Props {
   className?: string;
   id: string;
   label: string;
-  onChange: (value: string) => void;
-  options: { label: string; value: string }[];
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
+  options: {
+    label: string;
+    value: string;
+    disabled?: boolean;
+    selected?: boolean;
+  }[];
   placeholder?: string;
   required?: boolean;
   value: string;
@@ -21,34 +26,33 @@ const InputSelect: React.FC<Props> = ({
   placeholder,
   required,
   value
-}) => {
-  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    onChange(event?.currentTarget?.value || '');
-  };
-
-  return (
-    <div className={clsx('w-full', className)}>
-      <label htmlFor={id} className="block mb-2 font-bold text-gray">
-        {label}
-      </label>
-      <select
-        id={id}
-        className={
-          'w-full bg-white-100 rounded-md px-4 py-3 placeholder-gray-400 text-black-100 border-2 border-gray-300'
-        }
-        onChange={handleChange}
-        value={value}
-        placeholder={placeholder}
-        required={required}
-      >
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-};
+}) => (
+  <div className={clsx('w-full', className)}>
+    <label htmlFor={id} className="block mb-2 font-bold text-gray">
+      {label}
+    </label>
+    <select
+      id={id}
+      className={
+        'w-full bg-white-100 rounded-md px-4 py-3 placeholder-gray-400 text-black-100 border-2 border-gray-300'
+      }
+      onChange={onChange}
+      value={value}
+      placeholder={placeholder}
+      required={required}
+    >
+      {options.map((option) => (
+        <option
+          key={option.value}
+          value={option.value}
+          disabled={option.disabled}
+          selected={option.selected}
+        >
+          {option.label}
+        </option>
+      ))}
+    </select>
+  </div>
+);
 
 export default InputSelect;
