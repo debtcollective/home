68db3d43cef5771fec5be3d49dd36fc6ce47c8a1
arathjz <me@arathjz.com>
2021-05-25 20:36:40 -0500

    feat(sanity): restructure studio with new documents

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -5,15 +5,21 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // Document types
-import aboutUs from './documents/aboutUs';
+import aboutUsContent from './documents/aboutUsContent';
+import aboutUsHero from './documents/aboutUsHero';
+import debtorsUnion from './documents/debtorsUnion';
 import debtRelief from './documents/debtRelief';
+import donationHero from './documents/donationHero';
+import donationPageFeatures from './documents/donationPageFeatures';
 import homeHero from './documents/homeHero';
 import homePageFeatures from './documents/homePageFeatures';
-import ourTeam from './documents/ourTeam';
+import membershipBenefits from './documents/membershipBenefits';
+import ourTeamHero from './documents/ourTeamHero';
 import popup from './documents/popup';
+import teamMembers from './documents/teamMembers';
+import unionHero from './documents/unionHero';
 
 // Object types
-import aboutUsContent from './objects/aboutUsContent';
 import aboutUsItem from './objects/aboutUsItem';
 import badge from './objects/badge';
 import hero from './objects/hero';
@@ -29,19 +35,25 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    aboutUs,
     aboutUsContent,
+    aboutUsHero,
     aboutUsItem,
     badge,
+    debtorsUnion,
     debtRelief,
+    donationHero,
+    donationPageFeatures,
     hero,
     homeHero,
     homePageFeatures,
     link,
-    ourTeam,
+    membershipBenefits,
+    ourTeamHero,
     popup,
     popupImage,
     teamMember,
+    teamMembers,
+    unionHero,
     userAvatarImage
   ])
 });
