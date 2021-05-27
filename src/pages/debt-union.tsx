9f0f8791ab890268a62c64f9895fd8b3e61bad6c
arathjz <me@arathjz.com>
2021-05-27 18:26:58 -0500

    feat(debt-union): restructure union page with new sanity schema

--- src/pages/debt-union.tsx
+++ src/pages/debt-union.tsx
@@ -1,27 +1,48 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '@components/Layout';
 import WhatIsADebtorsUnion from '@sections/WhatIsADebtorsUnion';
 import MembershipBenefits from '@sections/MembershipBenefits';
 import FAQ from '@sections/FAQ';
 import PowerInNumbers from '@sections/PowerInNumbers';
 import OweTheBank from '@sections/OweTheBank';
-import YouAreNotALoan from '@sections/YouAreNotALoan';
+import DebtUnionCover from '@sections/DebtUnionCover';
+import {
+  ISanityDebtorsUnion,
+  ISanityUnionHero,
+  ISanityMembershipBenefits
+} from 'src/types/debt-union';
+
+interface Props {
+  data: {
+    sanityUnionHero: {
+      hero: ISanityUnionHero;
+    };
+    sanityDebtorsUnion: ISanityDebtorsUnion;
+    sanityMembershipBenefits: ISanityMembershipBenefits;
+  };
+}
 
 const metaTitle =
   'Alone our debts are a burden. Together they make us powerful';
 const metaDescription =
   'Join a growing community of debtors organizing to cancel debts and build financial and political power.';
 
-const DebtUnion: React.FC = () => {
+const DebtUnion: React.FC<Props> = ({ data }) => {
   return (
     <Layout title={metaTitle} description={metaDescription}>
-      <YouAreNotALoan />
-      <WhatIsADebtorsUnion />
+      <DebtUnionCover
+        title={data?.sanityUnionHero?.hero?._rawTitle}
+        body={data?.sanityUnionHero?.hero?.body}
+      />
+      <WhatIsADebtorsUnion
+        title={data?.sanityDebtorsUnion?.title}
+        body={data?.sanityDebtorsUnion?.body}
+      />
       <MembershipBenefits
-        title="Membership Benefits"
-        description="In addition to becoming part of a community of debtors and helping to
-        build the union, members also receive
-        access to a host of benefits:"
+        title={data?.sanityMembershipBenefits?.title}
+        description={data?.sanityMembershipBenefits?.body}
+        badges={data?.sanityMembershipBenefits?.badges}
       />
       <PowerInNumbers />
       <OweTheBank />
@@ -29,5 +50,36 @@ const DebtUnion: React.FC = () => {
     </Layout>
   );
 };
+
+export const query = graphql`
+  {
+    sanityUnionHero {
+      hero {
+        _rawTitle
+        body
+      }
+    }
+    sanityDebtorsUnion {
+      title
+      body
+    }
+    sanityMembershipBenefits {
+      title
+      body
+      badges {
+        icon {
+          asset {
+            url
+          }
+        }
+        title
+        body
+        color
+        href
+        caption
+      }
+    }
+  }
+`;
 
 export default DebtUnion;
