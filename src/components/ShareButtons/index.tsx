3f4eef6fc9d41257d3da4e23a7d11e3e2db595ca
Thiago de Mello Bueno <thiago@debtcollective.org>
2021-06-04 15:12:03 -0400

    feat: share widget
    text color

--- src/components/ShareButtons/index.tsx
+++ src/components/ShareButtons/index.tsx
@@ -21,10 +21,10 @@ const ShareButtons: React.FC<Props> = (props) => {
   const cta:string = (props.shareCTA) ? props.shareCTA : "Share this ";
   return(
     <div className="post-meta-share-icons flex items-center justify-around mx-auto min-w-md max-w-lg my-6 pt-6 mt-12 border-t-2 border-green-500">
-      <h3 className="font-black leading-5 py-6 mb-0">{cta}</h3>
+      <h3 className="font-black leading-5 py-6 mb-0 text-green-400">{cta}</h3>
       <div className="max-w-1/3 flex justify-between space-x-4 ml-4 rounded-md px-6 py-4 bg-green-400">
         <EmailShareButton {... props}>
-          <span className="sr-only"> in an email to a friend!</span>
+          <span className="sr-only">Share this page in an email to a friend!</span>
           <FontAwesomeIcon className="text-white text-4xl h-8 hover:text-black" icon={faEnvelope} />
         </EmailShareButton>
           <FacebookShareButton {... props}>
