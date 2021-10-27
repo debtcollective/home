c6575d6262c3086d31fab4dd507f3eaf615a2ca3
Thiago de Mello Bueno <thiago@debtcollective.org>
2021-10-27 14:40:07 -0500

    dev: updating typescript for our-team to accomodate gatsby-plugin-sanity-image

--- src/sections/OurTeamContent/TeamMemberAvatar.tsx
+++ src/sections/OurTeamContent/TeamMemberAvatar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-//import Img from 'gatsby-image';
 import { ISanityTeamMember } from 'src/types/our-team';
 import Img from 'gatsby-plugin-sanity-image';
 
@@ -10,7 +9,7 @@ interface Props {
 const TeamMemberAvatar: React.FC<Props> = ({ member }) => (
   <div>
     <div className="w-32 h-32 m-2 mx-auto overflow-hidden transition duration-150 rounded-full shadow-md md:m-4 md:w-64 md:h-64 bg-white-100 hover:bg-yellow">
-      <Img {...member.avatar} className="object-cover w-full h-full" />
+      <Img {...member.avatar} className="object-cover w-full h-full" caption={member.avatar.caption }/>
     </div>
     <h3 className="mt-3 mb-0 text-base font-bold text-center text-black md:text-lg">
       {member.name}
